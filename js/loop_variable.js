adbd527d729d907e92d232d71adca1ef7fd5b93e
Administrator <yingfeng.z@163.com>
2017-04-22 18:04:51 +0800

    test

--- js/loop_variable.js
+++ js/loop_variable.js
@@ -18,12 +18,12 @@ while (i<=100){
 console.log(sum);
 */
 
-for(var i=1,sumPlus=0;i<=100;i++){
+/*for(var i=1,sumPlus=0;i<=100;i++){
     sumPlus+=i;
 }
 console.log("从1加到100的值为："+sumPlus);
 
-/*计算从1乘到100的值*/
+/!*计算从1乘到100的值*!/
 for(var sumMulti=1,m=1;m<=100;m++){
     sumMulti*=m;
 }
@@ -32,7 +32,7 @@ console.log("从1乘到100的值为："+sumMulti);
 for(var n=1,sum=0;n<=999;n+=2){
     sum+=1/n;
 }
-console.log(sum);
+console.log(sum);*/
 
 /*控制台打印乘法口诀表
 * 1X1=1    第1行
@@ -43,7 +43,7 @@ console.log(sum);
 *                      打印第l行第1个,第2个...第j个 条件j<=i
 *                      乘数从1到i i<=l 被乘数一直为l
 * */
-window.onload=function () {
+/*window.onload=function () {
     var printHang=document.getElementById("printHang");
     printHang.onclick=function () {
 
@@ -56,9 +56,9 @@ window.onload=function () {
             console.log(str);
         }
     };
-};
+};*/
 /*直接table中打印99乘法表*/
-document.write("<table>");
+/*document.write("<table>");
 for(var x=1;x<=9;x++){
 document.write("<tr>");
     for(var y=1;y<=x;y++){
@@ -66,18 +66,97 @@ document.write("<tr>");
     }
     document.write("</tr>")
 }
-document.write("</table>");
+document.write("</table>");*/
 
 /*获得5个闰年*/
-var year=prompt("输入年份");
+/*var year=prompt("输入年份");
 var getLeapYear=parseInt(year);
 for(var yearMun=0;yearMun<5;yearMun==0?getLeapYear++:getLeapYear+=4){
     if(getLeapYear%4==0&&getLeapYear%100!=0||getLeapYear%400==0){
         console.log(getLeapYear);
         yearMun++;
     }
+}*/
+/*打印正三角
+*     *
+*     **
+*     ***
+*     ****
+*     *****
+* */
+function print(l) {
+    for(var i=1,str="";i<=l;i++){
+        str+="*";
+        console.log(str);
+    }
+}
+/*function print(l) {
+    for(var i=1;i<=l;i++){
+        for(var j=1;j<=i;j++){
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+}*/
+/*打印反三角
+*        *
+*       **
+*      ***
+*     ****
+*    *****
+* */
+/*function print2(l) {
+    for(var i=1;i<=l;i++){
+        for(var j=1;j<=l-i;j++){
+            document.write("&nbsp");
+        }
+        for(var k=1;k<=i;k++){
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+}*/
+function print2(l) {
+    l=parseInt(l);
+    for(var i=1;i<=l;i++){
+        var space=l-i;
+        var str="";
+        for(var j=0;j<l;j++){
+            if(j<space){
+                str+=" ";
+            }else{
+                str+="*";
+            }
+        }
+        console.log(str);
+    }
+}
+/*打印等腰三角
+*       *       1 1
+*      ***      2  3
+*     *****     3  5
+*    *******    4  7
+*   *********
+* */
+/*for(var i=1;i<=5;i++){
+ for(var j=1;j<=5-i;j++){
+ document.write("&nbsp");
+ }
+ for(var k=1;k<=2*i-1;k++){
+ document.write("*");
+ }
+ document.write("<br>");
+ }*/
+for(var i=1;i<=5;i++){
+    var str="";
+    var space=5-i;
+    for(var j=0;j<5+i-1;j++){
+        if(j<space){
+           str+="";
+        }else{
+           str+="*";
+        }
+    }
+    console.log(str);
 }
 
-
-
-//    找出从第1号学员到第500号学员中成绩大于90的学员；
